e2cf7b06012db5ba732e156a9684776b0dc0c9e5
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-23 19:37:00 +1100

    Create area.py

---
+++ area.py
@@ -0,0 +1,31 @@
+def square(w):
+    return w*w
+
+def rectangle(h, w):
+    return h*w
+
+def trangle_no_height(a, b):
+    h = (a**2-(b/2)**2)**0.5
+    return h*b*2
+
+def triangle(h, b):
+    return h*b/2
+
+def circle(r):
+    return 3.14159*r**2
+
+def oval(a, b):
+    return 3.14159*a*b
+
+def hexagon(s):
+    return (3*s**2(3**0.5))/2
+
+def parallelogram(b, h):
+    return b*h
+
+def parallelogram_no_height(a, b):
+    h = (b**2+a**2)**0.5
+    return b*h
+
+def trapezium(a, b, h):
+    return (a+b)/2*h
