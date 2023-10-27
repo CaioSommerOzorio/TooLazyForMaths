eac39394cb4eaea60c695b97fd33cbb666ab14ab
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-27 18:42:23 +1100

    Create allLogs.py

---
+++ allLogs.py
@@ -0,0 +1,24 @@
+import math
+import matplotlib.pyplot as plt
+
+x_axis = []
+y_axis = []
+
+def log_func():
+    min_x = int(input("Enter min x: "))
+    max_x = int(input("Enter max x: "))
+    for i in range(min_x, max_x+1):
+        y = math.log(i, 10)
+        x_axis.append(i)
+        y_axis.append(y)
+
+log_func()
+
+for i in range(0, len(x_axis)):
+    print(f"x: {x_axis[i]}\ny: {y_axis[i]}\n")
+
+plt.ylabel("y")
+plt.xlabel("x")
+plt.title("Logarithmic function")
+plt.plot(x_axis, y_axis)
+plt.show()
