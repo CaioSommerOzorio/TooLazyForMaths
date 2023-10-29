8d06c18934b4c13dc89a6476eff21e7833e77efc
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-29 18:11:57 +1100

    Delete midpoint.py

--- midpoint.py
+++
@@ -1,8 +0,0 @@
-def midpoint():
-	x1 = input("Enter x1: ")
-	x2 = input("Enter x2: ")
-	y1 = input("Enter y1: ")
-	y2 = input("Enter y2: ")
-	x3 = int(x1)+int(x2)
-	y3 = int(y1)+int(y2)
-	return f"({int(x3)/2}, {int(y3)/2})"
