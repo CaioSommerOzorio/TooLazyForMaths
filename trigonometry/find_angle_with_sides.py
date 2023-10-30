87fc6278abebc838a00448957860f538fd1d1f6c
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-30 20:06:07 +1100

    Create find_angle_with_sides.py

---
+++ trigonometry/find_angle_with_sides.py
@@ -0,0 +1,30 @@
+import math
+
+#   │╲
+#   │y╲
+#   │  ╲ 
+#   │   ╲
+# a │    ╲ c
+#   │     ╲
+#   │      ╲
+#   ├┐     x╲
+#   └┴───────┘
+#        b
+
+def find_x_ac(a, c):
+    return math.degrees(math.asin(a/c))
+
+def find_x_ab(a, b):
+    return math.degrees(math.atan(a/b))
+
+def find_x_bc(b, c):
+    return math.degrees(math.acos(b/c))
+
+def find_y_ac(a, c):
+    return math.degrees(math.acos(a/c))
+
+def find_y_ab(a, b):
+    return math.degrees(math.atan(b/a))
+
+def find_y_bc(b, c):
+    return math.degrees(math.asin(b/c))
