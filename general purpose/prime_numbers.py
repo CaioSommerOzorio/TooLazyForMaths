ba754c578f2b3eddc33060a2cd276a28f8745f02
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-11-02 09:59:57 +1100

    Removed unnecessary comments

--- general purpose/prime_numbers.py
+++ general purpose/prime_numbers.py
@@ -1,27 +1,11 @@
 def find_prime_numbers(ceiling):
     prime_numbers = [2]
     for i in range(2, ceiling+1):
-        #print("i", i)
         for j in prime_numbers:
-            #print("j", j)
-            #print(f"Checking if {i} is divisible by {j}.")
             if i % j == 0:
                 if i != j:
-                    #print("It is.")
-                    #print(prime_numbers)
-                    #print()
                     break
-                #print("They're the same number.")
-                #print(prime_numbers)
-                #print()
             else:
-                #print("It is not")
                 if i not in prime_numbers and j == prime_numbers[-1]:
-                    #print("Appending", i)
                     prime_numbers.append(i)
-                #print(prime_numbers)
-                #print()
     return prime_numbers
-
-
-print(find_prime_numbers(500))
